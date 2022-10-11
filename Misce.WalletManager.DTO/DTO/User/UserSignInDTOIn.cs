3823ff80570a2400062243f677ea07b196940a38
Michael Tagliati <m.tagliati@idigital3.com>
2022-10-11 11:04:34 +0200

    regex for password

--- Misce.WalletManager.DTO/DTO/User/UserSignInDTOIn.cs
+++ Misce.WalletManager.DTO/DTO/User/UserSignInDTOIn.cs
@@ -7,7 +7,7 @@ namespace Misce.WalletManager.DTO.DTO.User
         [Required(AllowEmptyStrings = false, ErrorMessage = "The username has to be provided")]
         public string Username { get; init; } = null!;
         [Required(ErrorMessage = "The password has to be provided")]
-        [MinLength(8, ErrorMessage = "The password has to be at least 8 characters long")]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$", ErrorMessage = "The password must be at least 8 characters long, contain an uppercase and a lowercase letter, and a number ")]
         public string Password { get; init; } = null!;
     }
 }
--- Misce.WalletManager.Test/ServiceTests/UserServiceTest.cs
+++ Misce.WalletManager.Test/ServiceTests/UserServiceTest.cs
@@ -24,7 +24,7 @@ namespace Misce.WalletManager.Test.ServiceTests
 
         [TestMethod]
         [ExpectedException(typeof(IncorrectDataException))]
-        public void TestFailingSignIn()
+        public void TestFailingSignIn1()
         {
             //initialize the services
             var userService = new UserService(_dbContext);
@@ -33,7 +33,23 @@ namespace Misce.WalletManager.Test.ServiceTests
             var newUser = new UserSignInDTOIn
             {
                 Username = "gigidag",
-                Password = "wella1!"
+                Password = "Wella1!"
+            };
+            userService.RegisterUser(newUser);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IncorrectDataException))]
+        public void TestFailingSignIn2()
+        {
+            //initialize the services
+            var userService = new UserService(_dbContext);
+
+            //try to create the new user with a password without a number
+            var newUser = new UserSignInDTOIn
+            {
+                Username = "gigidag",
+                Password = "wellaAAAAAAAAAAAAAAA"
             };
             userService.RegisterUser(newUser);
         }
@@ -49,18 +65,18 @@ namespace Misce.WalletManager.Test.ServiceTests
             var newUser = new UserSignInDTOIn
             {
                 Username = "gigidag",
-                Password = "wellAAAAAAAAAAA1!"
+                Password = "Maritte0!?"
             };
             userService.RegisterUser(newUser);
 
             //verify that the new user can now log in
-            Assert.IsNotNull(userService.Authenticate(new UserLoginDTOIn { Username = "gigidag", Password = "wellAAAAAAAAAAA1!" }));
+            Assert.IsNotNull(userService.Authenticate(new UserLoginDTOIn { Username = "gigidag", Password = "Maritte0!?" }));
 
             //try to create the new user with an already taken username
             newUser = new UserSignInDTOIn
             {
                 Username = "gigidag",
-                Password = "sdvlisw_esyivHGYUKF"
+                Password = "sdvlisw_esyivHGYU2KF"
             };
             userService.RegisterUser(newUser);
         }
