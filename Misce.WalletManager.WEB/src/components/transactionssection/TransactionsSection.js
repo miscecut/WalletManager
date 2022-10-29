1b4d1911ee9eb6de4e0113c7cea5e7e3e30de861
Michael Tagliati <67690109+miscecut@users.noreply.github.com>
2022-10-29 18:04:34 +0200

    tot profit and expense in sections

--- Misce.WalletManager.WEB/src/components/transactionssection/TransactionsSection.js
+++ Misce.WalletManager.WEB/src/components/transactionssection/TransactionsSection.js
@@ -3,13 +3,42 @@ import React from 'react';
 import './TransactionsSection.css';
 //components
 import Transaction from './../transaction/Transaction.js'
+//utils
+import { formatMoneyAmount } from './../../jsutils/beautifiers.js';
 
 function TransactionsSection({ title, transactions }) {
+
+    //FUNCTIONS
+
+    const getTotalProfit = () => {
+        let tot = 0;
+        transactions.forEach(t => {
+            if (t.fromAccount === null && t.toAccount !== null)
+                tot += t.amount;
+        });
+        return tot;
+    }
+
+    const getTotalExpense = () => {
+        let tot = 0;
+        transactions.forEach(t => {
+            if (t.fromAccount !== null && t.toAccount === null)
+                tot += t.amount;
+        });
+        return tot;
+    }
+
+    //RENDERING
+
     //component render
     return <div className="misce-transactions-section">
         <div className="misce-transactions-section-header">
             <hr className="misce-transactions-section-header-line"></hr>
-            <p className="misce-transactions-section-header-title">{title}</p>
+            <div className="misce-transactions-section-header-title">
+                <p>{title}</p>
+                <p className="tot-profit">{formatMoneyAmount(getTotalProfit())} &euro;</p>
+                <p className="tot-expense">{formatMoneyAmount(getTotalExpense())} &euro;</p>
+            </div>
             <hr className="misce-transactions-section-header-line"></hr>
         </div>
         {transactions.map(t => <Transaction key={t.id} transaction={{ ...t, dateTime: new Date(t.dateTime).toLocaleString().replace(',', ' -') }} />)}
