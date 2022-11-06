f80559f6fa5b6eeb727c961949d7c8d21f4930cb
Michael Tagliati <67690109+miscecut@users.noreply.github.com>
2022-11-06 14:25:12 +0100

    tc style fix

--- Misce.WalletManager.WEB/src/components/transactioncategories/transactioncategory/TransactionCategory.js
+++ Misce.WalletManager.WEB/src/components/transactioncategories/transactioncategory/TransactionCategory.js
@@ -4,8 +4,8 @@ import './TransactionCategory.css'
 
 function TransactionCategory({ transactionCategory, editClick }) {
     //render component
-    return <div className="misce-transaction-category">
-        <p className={`misce-transaction-category-name ${transactionCategory.isExpenseType ? 'expense' : 'profit'}`}>{transactionCategory.name}</p>
+    return <div className={`misce-transaction-category ${transactionCategory.isExpenseType ? 'expense' : 'profit'}`}>
+        <p className="misce-transaction-category-name">{transactionCategory.name}</p>
         <div className="misce-transaction-category-actions">
             <button className="misce-action-button misce-edit-button" type="button" onClick={() => editClick(transactionCategory.id)}></button>
             <button className="misce-action-button misce-delete-button" type="button"></button>
