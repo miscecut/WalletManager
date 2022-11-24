9e5c634b8e9b8f8310c93ad7f43e40cca2fde46a
Michael Tagliati <m.tagliati@idigital3.com>
2022-11-24 14:12:32 +0100

    account history chart

--- Misce.WalletManager.WEB/src/components/accounts/account/Account.js
+++ Misce.WalletManager.WEB/src/components/accounts/account/Account.js
@@ -1,10 +1,50 @@
 import React from 'react';
+//chart-js-2
+import { Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
 //css
 import './Account.css';
 //utils
 import { formatMoneyAmount } from './../../../jsutils/beautifiers.js';
 
+//chart-js-2 setup
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+//component
 function Account(props) {
+
+    //LINE GRAPH DATA & OPTIONS
+    const chartData = {
+        labels: props.account.accountAmountHistory.map(val => val.atDate),
+        datasets: [
+            {
+                label: 'Amount',
+                backgroundColor: '#ea80fc',
+                borderColor: '#ea80fc',
+                data: props.account.accountAmountHistory.map(val => val.amount)
+            }
+        ]
+    };
+    const chartOptions = {
+        maintainAspectRatio: false
+    };
 
     //FUNCTIONS
 
@@ -25,6 +65,13 @@ function Account(props) {
         <div className="misce-account-card-info-container">
             <p className={`misce-account-name ${props.account.isActive ? '' : 'inactive'}`}>{props.account.name}</p>
             <p className={`misce-account-amount ${props.account.actualAmount >= 0 ? 'positive' : 'negative'}`}>{formatMoneyAmount(props.account.actualAmount)} &euro;</p>
+        </div>
+        <div>
+            <Line
+                data={chartData}
+                options={chartOptions}
+                height={'170px'}
+            />
         </div>
     </div>
 }
