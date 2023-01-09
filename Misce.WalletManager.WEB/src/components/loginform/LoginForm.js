4dcbbc6d1a7c0534f3bb53b287b6e62c578632a9
Michael Tagliati <m.tagliati@idigital3.com>
2023-01-09 09:23:06 +0100

    Update LoginForm.js

--- Misce.WalletManager.WEB/src/components/loginform/LoginForm.js
+++ Misce.WalletManager.WEB/src/components/loginform/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+//utils
 import { getErrorMap } from "../../jsutils/errorhandling.js"
 
 function LoginForm({ login, loginErrors }) {
@@ -11,6 +12,7 @@ function LoginForm({ login, loginErrors }) {
     //FUNCTIONS
 
     const submitHandler = e => {
+        console.log(e.target);
         e.preventDefault();
         login(details);
     }
@@ -21,20 +23,20 @@ function LoginForm({ login, loginErrors }) {
 
     //render component
     return <form onSubmit={submitHandler} className="misce-central-form-container">
-            <h1 className="misce-login-title">Welcome</h1>
-            <div className="misce-input-container">
-                <label className="misce-input-label" htmlFor="username">Username:</label>
-                <input className="misce-input" type="text"  name="username" id="username" onChange={e => setDetails({ ...details, username: e.target.value })} value={details.username} required></input>
-            </div>
-            <div className="misce-input-container">
-                <label className="misce-input-label" htmlFor="password">Password:</label>
-                <input className={`misce-input ${errorMap['password'] != null ? 'misce-input-error' : ''}`} type="password" name="password" id="password" onChange={e => setDetails({ ...details, password: e.target.value })} value={details.password} required></input>
-                <p className="misce-input-error-message">{errorMap['password']}</p>
-            </div>
-            <div className="misce-input-container mt-more">
-                <button className="misce-btn" type="submit">LOGIN</button>
-            </div>
-        </form>
+        <h1 className="misce-login-title">Welcome</h1>
+        <div className="misce-input-container">
+            <label className="misce-input-label" htmlFor="username">Username:</label>
+            <input className="misce-input" type="text"  name="username" id="username" onChange={e => setDetails({ ...details, username: e.target.value })} value={details.username} required></input>
+        </div>
+        <div className="misce-input-container">
+            <label className="misce-input-label" htmlFor="password">Password:</label>
+            <input className={`misce-input ${errorMap['password'] != null ? 'misce-input-error' : ''}`} type="password" name="password" id="password" onChange={e => setDetails({ ...details, password: e.target.value })} value={details.password} required></input>
+            <p className="misce-input-error-message">{errorMap['password']}</p>
+        </div>
+        <div className="misce-input-container mt-more">
+            <button className="misce-btn" type="submit">LOGIN</button>
+        </div>
+    </form>
 }
 
 export default LoginForm;
